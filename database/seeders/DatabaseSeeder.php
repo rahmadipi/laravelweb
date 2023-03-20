0cde32f42180f60837204c7ed26ecc38c6efbf4d
Putra R <putrarahmadi615@gmail.com>
2023-03-20 16:51:07 +0700

    20-3 faker test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //User::factory(10)->create();
+        User::factory(10)->create();
+
+        /*
         Category::create([
             'name' => 'News',
             'slug' => 'news',
@@ -30,5 +32,6 @@ class DatabaseSeeder extends Seeder
             'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores accusamus rerum nam ex iste beatae consequuntur eaque aliquid dolorem magni aut modi.',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores accusamus rerum nam ex iste beatae consequuntur eaque aliquid dolorem magni aut modi, provident, in, voluptatum dignissimos commodi hic minus maxime veniam esse aspernatur. Ducimus, suscipit nulla. Exercitationem voluptatem hic sunt numquam nesciunt reprehenderit aperiam praesentium quaerat, quidem aliquid veniam eius quos fugiat natus consequatur obcaecati? Soluta, quisquam aspernatur dignissimos ab rerum optio. Assumenda reprehenderit natus, facere nulla minus ea magnam saepe ad non aspernatur similique vero corporis sint repellat quisquam sequi nisi soluta laboriosam incidunt eius aperiam possimus dolorem, veritatis facilis? Alias maxime nulla ullam, enim odio et cumque non?'
         ]);
+        */
     }
 }
