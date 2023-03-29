ba3d7f8d0951723c69559f751b1d0e57da276318
rahmadipi <putrarahmadi615@gmail.com>
2023-03-29 20:27:05 +0700

    29-3 fix add icon

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,4 +1,4 @@
-{{-- <script src="{{ url('/vendor/jquery-3.6.4/jquery-3.6.4.min.js') }}"></script> --}}
-<script src="{{ asset('vendor/components/jquery/jquery.min.js') }}"></script>
-{{-- <script src="{{ url('/vendor/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js') }}"></script> --}}
-<link href="{{ asset('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js') }}" rel="stylesheet">
+<script src="{{ url('/vendor/jquery-3.6.4/jquery-3.6.4.min.js') }}"></script>
+{{-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script> --}}
+<script src="{{ url('/vendor/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js') }}"></script>
+{{-- <script src="{{ asset('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script> --}}
